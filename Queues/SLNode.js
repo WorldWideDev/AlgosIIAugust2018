5bcf016046e921e8a8c94d8d9f814ba524ab8d29
= <=>
2018-07-30 11:40:04 -0700

    updated with better code

--- Queues/SLNode.js
+++ Queues/SLNode.js
@@ -11,24 +11,36 @@ function SLQueue(){
         return this.head === null;
     }
     this.enqueue = function(val){
+
+        // WE were going down a bad path!!!
+        // Heres a possible way we can do this
+        
         // 1) init new node with val
         var newNode = new SLNode(val);
 
-        // 2) point current tail.next to new node
-        if(!this.isEmpty){
-            this.tail.next = newNode;
-            if(this.head.next === null)
-                this.head.next = newNode;
-        }
-        else {
+        // 2) point tail to new node
+        this.tail = newNode;
+    
+
+        // 3) add new node to end of list
+        
+        // first check for empty queue
+        if(this.isEmpty()){
             this.head = newNode;
+            return this;
+
         }
 
-        // 3) point tail to new node
-        this.tail = newNode;
+        // then traverse to end of queue, add new item at end
+        var curr = this.head;
+        while(curr.next){
+            curr = curr.next;
+        }
+        curr.next = newNode;
 
         return this;
     }
 }
 
-
+var test = new SLQueue();
+test.enqueue(1).enqueue(2);
